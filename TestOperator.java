4f047b45b7b93eca9f7a1ff251f700cb1f935833
justforheart <1491516376@qq.com>
2018-06-24 17:59:21 +0800

    新增TestOperator.java 测试验证位运算

--- TestOperator.java
+++ TestOperator.java
@@ -1,7 +1,21 @@
 public class TestOperator {
 	public static void main(String[] args) {
 		int a = 10;
+		int c = 5;
+		
 		System.out.println("a = " + Integer.toBinaryString(a));
+		System.out.println("c = " + Integer.toBinaryString(c));
+		System.out.println("取反运算~a = " + Integer.toBinaryString(~a));
+		System.out.println("与运算a&c = " + Integer.toBinaryString(a&c));
+		System.out.println("或运算a|c = " + Integer.toBinaryString(a|c));
+/*
+		if(~b) {
+			System.out.println("b为假！");
+		}else {
+			System.out.println("b为真！");
+		}
+*/
+		System.out.println("异或运算a^c = " + Integer.toBinaryString(a^c));
 		a = a << 1;
 		System.out.println("a左移一位后：a = " + Integer.toBinaryString(a));
 		a = a >> 2;

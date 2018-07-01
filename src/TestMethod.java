23e2e98241de8ae5334e8ee945aae9af0f5a8892
justforheart <1491516376@qq.com>
2018-07-01 11:59:17 +0800

    新文件：   bin/TestMethod.class 	新文件：   src/TestMethod.java 	修改：     .gitignore

---
+++ src/TestMethod.java
@@ -0,0 +1,25 @@
+
+public class TestMethod {
+	public static void main(String[] args) {
+		int c = 0;
+		
+		//通过对象调用方法
+		TestMethod tm = new TestMethod();
+		tm.MyPrint();	//调用无参方法
+		c = tm.MyAdd(4, 5, 6);	//调用有参方法。 用 c 存储返回的结果
+		System.out.println(c);
+	}
+	
+	// 定义一个无参的方法
+	void MyPrint() {
+		System.out.println("我要打印一行字。");
+	}
+	
+	// 定义一个有参的方法，返回一个int类型的变量
+	int MyAdd(int a, int b, int c) {
+		int sum = 0;
+		sum = a + b + c;
+		
+		return sum;
+	}
+}

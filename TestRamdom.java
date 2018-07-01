2fb23d4c9f48a69998db49592ce979e0396d0e7b
justforheart <1491516376@qq.com>
2018-07-01 10:36:45 +0800

    add TestRamdom.java

---
+++ TestRamdom.java
@@ -0,0 +1,15 @@
+
+public class TestRamdom {
+	public static void main(String[] args) {
+		double r = Math.random();
+		System.out.println(r);
+		int a = (int)(r * 10) + 1;
+		
+		System.out.println(a);
+		if (a > 3) {
+			System.out.println("运气不错");
+		}else {
+			System.out.println("点数小");
+		}
+	}
+}
